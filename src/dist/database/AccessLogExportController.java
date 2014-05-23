6b51bca93d0e0945a67f6cdba6bf152bd69da27a
DaiAkiba <gaslakiba@gmailcom>
2014-05-23 18:29:26 +0900

    change query by bat argument

--- src/dist/database/AccessLogExportController.java
+++ src/dist/database/AccessLogExportController.java
@@ -9,8 +9,10 @@ import java.io.FileOutputStream;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.Calendar;
 
 import au.com.bytecode.opencsv.CSVWriter;
 
@@ -28,8 +30,18 @@ public class AccessLogExportController {
 	
 	public int export(Connection connection) throws Exception {
 		csvWriter = setupCsvWriter();
-		
-		try (PreparedStatement statement = connection.prepareStatement(makeSQL())) {
+		final String baseQuery = "SELECT * FROM SctAccessLog acl"
+				+ " LEFT JOIN Revisions rev on (acl.sc_scs_dID = rev.dID)"
+				+ " LEFT JOIN DocMeta doc on (acl.sc_scs_dID = doc.dID)"
+				+ " WHERE acl.eventDate > ? AND acl.eventDate < ?"
+				+ " ORDER BY acl.eventDate %s";
+		String query = String.format(baseQuery, getOrderByArgument(""));
+		try (PreparedStatement statement = connection.prepareStatement(query)) {
+			String strStartDate = "20140401";
+			String strEndDate = "20140531";
+			
+			statement.setDate(1, getDateByArgument(strStartDate));
+			statement.setDate(2, getDateByArgument(strEndDate));
 			try (ResultSet resultSet = statement.executeQuery()) {
 				csvWriter.writeAll(resultSet, true);
 			}
@@ -46,8 +58,22 @@ public class AccessLogExportController {
 		return new CSVWriter(writer,CSVWriter.DEFAULT_SEPARATOR ,CSVWriter.DEFAULT_QUOTE_CHARACTER,"\r\n");
 	}
 	
-	private String makeSQL() {
-		String strSQL = "SELECT * FROM DocMeta";
-		return strSQL;
+	private String getOrderByArgument(String orderArgument) {
+		String order = "ASC";
+		if (orderArgument.length() != 0) {
+			order = orderArgument;
+		}
+		return order;
+	}
+	
+	private Date getDateByArgument(String dateArgument) {
+		Integer year = Integer.parseInt(dateArgument.substring(0, 4));
+		Integer month = Integer.parseInt(dateArgument.substring(4,6));
+		Integer day = Integer.parseInt(dateArgument.substring(6, 8));
+		
+		Calendar calDate = Calendar.getInstance();
+		calDate.set(year,month-1,day,0,0,0);
+		
+		return new Date(calDate.getTimeInMillis());
 	}
 }
